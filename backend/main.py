5553539e023b9a5060a48649f707102b0b36031f
kalvpsia <aristemmerath@gmail.com>
2023-11-10 21:41:43 -0600

    implemented download pdf function

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import requests
 
 def pdf_to_text(pdf_path):
     text = ""
@@ -13,6 +14,23 @@ def pdf_to_text(pdf_path):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
     return text
+
+def download_pdf(url, output_file):
+    try:
+        # Send an HTTP GET request to the server
+        response = requests.get(url)
+        
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            # Open the output file in binary write mode
+            with open(output_file, 'wb') as pdf_file:
+                # Write the content of the response to the file
+                pdf_file.write(response.content)
+            print(f"Downloaded PDF saved as {output_file}")
+        else:
+            print(f"Failed to download PDF. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     # Replace 'your_pdf_file.pdf' with the actual path to your PDF file
