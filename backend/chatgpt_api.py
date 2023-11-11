161d0473deb93d0f537130192be126aa007f6a53
kalvpsia <aristemmerath@gmail.com>
2023-11-11 16:38:08 -0600

    some optimizing
    i left the API key out. will need to get the key from me and run this on your local machine.

--- backend/chatgpt_api.py
+++ backend/chatgpt_api.py
@@ -1,21 +1,27 @@
 import openai
-import os
-import pandas as pd
-import time
 
-openai.api_key = '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'
+class ChatGPT_API:
+    def __init__(self, api_key, model="gpt-3.5-turbo"):
+        self.api_key = api_key
+        self.model = model
+        openai.api_key = api_key
+        
+    def get_completion(self, prompt):
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model = self.model,
+            messages = messages,
+            temperature = 0,
+        )
+        return response.choices[0].message["content"]
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
-    )
-    return response.choices[0].message["content"]
+if __name__ == "__main__":
+    api_key = ''
 
-prompt = "What is the current temperature in St. Cloud, MN?"
+    chatbot = ChatGPT_API(api_key)
 
-response = get_completion(prompt)
+    prompt = "Who won the Grammy for best rock album in 2020?"
 
-print(response)
+    response = chatbot.get_completion(prompt)
+
+    print(response)
