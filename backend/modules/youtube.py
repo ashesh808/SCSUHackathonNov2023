08e001cd1af842d65a9f764e03739839d5b741fd
Matthew Molenaar <tntmaster38@gmail.com>
2023-11-11 14:29:37 -0600

    fixed having the same flie in two locations

--- backend/modules/youtube.py
+++ backend/modules/youtube.py
@@ -1,20 +1,31 @@
 from yt_dlp import YoutubeDL
 import os
 
-## args writesubtitles, subtitleformat, skip_download
+class Youtube:
 
-dlpath = {
-    'home' : 'backend\\documents\\Youtube Data'
-}
+    def __init__(self, fileName='test', URL='https://www.youtube.com/watch?v=dQw4w9WgXcQ'):
+        self.file = fileName
+        self.url = URL
+        self.path = {'home' : 'backend\\data\\youtuberawdata'}
 
-ydl_opts = {
-    'writesubtitles' : True,
-    'forcefilename' : True,
-    'skip_download' : True,
-    'paths' : dlpath
-}
+    def Download(self):
+        output = {'default' : self.file}
 
-URL = ['https://www.youtube.com/watch?v=j3OqAN4ISOw']
-with YoutubeDL(ydl_opts) as ydl :
-    ydl.download(URL)
+        ydl_opts = {
+            'writesubtitles' : True,
+            'writeautomaticsub' : True,
+            'forcefilename' : True,
+            'skip_download' : True,
+            'paths' : self.path,
+            'outtmpl' : output
+        }
+
+        URL = [self.url]
+
+        with YoutubeDL(ydl_opts) as ydl :
+            ydl.download(URL)
+
+if __name__ == '__main__':
+    Test = Youtube(URL='https://www.youtube.com/watch?v=gXyrQ7FNIKk&list=PLliBvQE3gg9cA3zMs2b0gokZVsLWtvcGu&pp=iAQB')
+    Test.Download()
     
