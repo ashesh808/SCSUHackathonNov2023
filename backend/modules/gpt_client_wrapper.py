46b4ab5a1334eb3010d1af8722237357847d0c9d
Ashesh Nepal <asheshusa9@gmail.com>
2023-11-11 17:42:37 -0600

    Completed sendQuery module need to better format

--- backend/modules/gpt_client_wrapper.py
+++ backend/modules/gpt_client_wrapper.py
@@ -17,9 +17,4 @@ class GPTClientWrapper:
                 {"role": "user", "content": prompt}
             ]
         )
-        print(completion.choices[0].message.content)
-
-if __name__ == "__main__":
-    gpt_wrapper = GPTClientWrapper()
-    prompt = "In the early twentieth century, the US was deeply influenced by the eugenics movement, which espoused the idea of improving the genetic quality of the population through selective breeding and sterilization. Pseudo-scientific notions of racial superiority and inferiority played a significant role in shaping the US ethnic landscape during this period. In Montrie's book, it is stated that “ ‘The master race of the world is Caucasian,’ Buck declared in his polemic’s opening lines, and that dominance was sustained, he claimed, by a singular devotion to both ‘Civilization and Christianity’ ”(Montrie, 2). Statements like these and the use of phrases like “The master race” show what the perception of ethnic communities may have been in the early twentieth century. These theories not only contributed to a sense of superiority among white Americans but also fueled discrimination and segregation against non-white communities, particularly African Americans."
-    gpt_wrapper.get_flashcards_json(prompt)
+        return completion.choices[0].message.content
